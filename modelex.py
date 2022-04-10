b132cea4162136c0092b2725df9529a96cb4b5e3
AsmeretT <98982365+AsmeretT@users.noreply.github.com>
2022-04-10 15:52:03 -0400

    Asmeret Wk2Day3HW

---
+++ modelex.py
@@ -0,0 +1,17 @@
+import math 
+from math import pi
+
+def houseFunc():
+    width1 = int(input("what is the width of your home?"))
+    length1 = int(input("what is the length of your home?"))
+    squared_area = width1 * length1
+    print(squared_area)
+
+def circFunc():
+    math.pi
+    d = int(input("what is the diameter of your circle"))
+    circf = pi * d
+    print(circf)
+
+
+    
